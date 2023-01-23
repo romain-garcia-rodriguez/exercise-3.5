331126be68f20a9041957532f2831e120da7bed1
romain-garcia-rodriguez <romain.garcia@hotmail.com>
2023-01-23 11:44:30 +0100

    Init project

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+
+<template>
+  <!-- Créer un convertisseur Celsius/Fahrenheit en utilisant la propriété watch de Vue.
+  Lorsque l'utilisateur entrera une valeur en degré Celsius, il verra directement la conversion en Fahrenheit (et inversement) -->
+  <main></main>
+</template>
+
+<style scoped></style>
